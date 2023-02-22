9b97db4671e0b82b5fdce94b0f88453cfd267ee4
Kissa41 <nastyagoncharenko5@gmail.com>
2023-02-22 19:44:54 +0100

    add php mailer

--- scripts/script.js
+++ scripts/script.js
@@ -15,6 +15,16 @@ inputPhoneElement.addEventListener('input', (e) => {
     inputPhoneElement.value = e.target.value.slice(0, 12)
 })
 
+inputPhoneElement.addEventListener('focus', _ => {
+    if(!/^\+\d*$/.test(inputPhoneElement.value))
+    inputPhoneElement.value = '';
+});
+
+inputPhoneElement.addEventListener('keypress', e => {
+  if(!/\d/.test(e.key))
+    e.preventDefault();
+});
+
 countryButton.addEventListener('click', (e) => {
     modal.classList.toggle('country-select__modal-open')
 })
@@ -81,7 +91,69 @@ const changeTheme = () => {
 
 th.addEventListener('click', changeTheme);
 
+"use strict"
 
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('form');
+    form.addEventListener('submit', formSend);
+
+    async function formSend(e){
+        e.preventDefault();
+
+        let error = formValidate(form);
+
+        let formData = new FormData(form);
+        if (error === 0) {
+            form.classList.add('_sending');
+            let response = await fetch('sendmail.php', {
+                method: 'POST',
+                body: formData
+            });
+            if (response.ok) {
+                let result = await response.json();
+                alert(result.message);
+                form.reset();
+                form.classList.remove('_sending');
+            }else{
+                alert("Error");
+                form.classList.remove('_sending');
+            }
+        }
+    }
+
+    function formValidate(form){
+        let error = 0;
+        let formReq = document.querySelectorAll('._req');
+
+        for (let i = 0; i < formReq.length; i++) {
+            const input = formReq[i];
+            formRemoveError(input);
+
+            if ((input.value.trim() === '') || (input.value == '')) {
+                formAddError(input);
+                error++; 
+            }else if (input.classList.contains('_email')) {
+                if (emailTest(input)){
+                    formAddError(input);
+                    error++;
+                }
+            }
+        }
+        return error;
+    }
+
+    function formAddError(input) {
+        input.parentElement.classList.add('_error');
+        input.classList.add('_error');
+    }
+    function formRemoveError(input) {
+        input.parentElement.classList.remove('_error');
+        input.classList.remove('_error');
+    }
+    function emailTest(input) {
+        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+    }
+});
 
 
 
---
+++ sendmail.php
@@ -0,0 +1,44 @@
+<?php
+use PHPMailer/PHPMailer/PHPMailer;
+use PHPMailer/PHPMailer/Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+
+$mail = new PHPMailer(true);
+$mail->CharSet = 'UTF-8'
+$mail->setLanguage('ru', 'phpmailer/language/');
+$mail->IsHTML(true);
+
+$mail->setFrom('nastyagoncharenko5@gmail.com', 'Гончаренко Анастасия');
+$mail->addAddress('richard00111010@gmail.com');
+$mail->Subject = '"Гончаренко Анастасия"';
+
+$body = '<h1>Встречайте супер письмо!</h1>';
+
+if(trim(!empty($_POST['name']))){
+    $body.='<p><strong>Name:</strong> '.$_POST['name'].'</p>';
+}
+if(trim(!empty($_POST['phone']))){
+    $body.='<p><strong>Phone:</strong> '.$_POST['phone'].'</p>';
+}
+if(trim(!empty($_POST['email']))){
+    $body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+}        
+if(trim(!empty($_POST['message']))){
+    $body.='<p><strong>Comment:</strong> '.$_POST['message'].'</p>';
+}
+
+$mail->Body = $body;
+
+if (!$mail->send()) {
+    $message = 'Error';
+} else {
+    $message = 'Succes!'
+}
+
+$response = ['message' => $message];
+
+header('Content-type: application/json')
+echo json_encode($response);
+?>
