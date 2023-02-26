13fcb84a836009885a02c33eb5893fb44abf068d
Kissa41 <nastyagoncharenko5@gmail.com>
2023-02-26 22:59:40 +0100

    some-fixes

--- scripts/script.js
+++ scripts/script.js
@@ -103,7 +103,8 @@ document.addEventListener('DOMContentLoaded', function () {
         let error = formValidate(form);
 
         let formData = new FormData(form);
-        if (error === 0) {
+
+        if (!error) {
             form.classList.add('_sending');
             let response = await fetch('sendmail.php', {
                 method: 'POST',
@@ -112,14 +113,49 @@ document.addEventListener('DOMContentLoaded', function () {
             if (response.ok) {
                 let result = await response.json();
                 alert(result.message);
+                formPreview.innerHTML = '';
                 form.reset();
                 form.classList.remove('_sending');
             }else{
-                alert("Error");
-                form.classList.remove('_sending');
+                alert("Ошибка");
+                //удалить класс сендинг
             }
+        }else{
+            alert('Заполните обязательные поля!');
         }
     }
+    
+
+    // async function formSend(e) {
+    //     e.preventDefault();
+    
+    //     let error = formValidate(form);
+    
+    //     let formData = new FormData(form);
+    //     if (error === 0) {
+    //         form.classList.add('_sending');
+    //         try {
+    //             let response = await fetch('sendmail.php', {
+    //                 method: 'POST',
+    //                 body: formData
+    //             });
+    //             if (response.ok) {
+    //                 let result = await response.json();
+    //                 alert(result.message);
+    //                 form.reset();
+    //                 form.classList.remove('_sending');
+    //             } else {
+    //                 alert("Error");
+    //                 form.classList.remove('_sending');
+    //             }
+    //         } catch (error) {
+    //             console.error(error);
+    //             alert("Errorjs");
+    //             form.classList.remove('_sending');
+    //         }
+    //     }
+    // }
+    
 
     function formValidate(form){
         let error = 0;
--- sendmail.php
+++ sendmail.php
@@ -1,44 +1,44 @@
 <?php
-use PHPMailer/PHPMailer/PHPMailer;
-use PHPMailer/PHPMailer/Exception;
+    use PHPMailer/PHPMailer/PHPMailer;
+    use PHPMailer/PHPMailer/Exception;
 
-require 'phpmailer/src/Exception.php';
-require 'phpmailer/src/PHPMailer.php';
+    require 'phpmailer/src/Exception.php';
+    require 'phpmailer/src/PHPMailer.php';
 
-$mail = new PHPMailer(true);
-$mail->CharSet = 'UTF-8'
-$mail->setLanguage('ru', 'phpmailer/language/');
-$mail->IsHTML(true);
+    $mail = new PHPMailer(true);
+    $mail->CharSet = 'UTF-8'
+    $mail->setLanguage('ru', 'phpmailer/language/');
+    $mail->IsHTML(true);
 
-$mail->setFrom('nastyagoncharenko5@gmail.com', 'Гончаренко Анастасия');
-$mail->addAddress('richard00111010@gmail.com');
-$mail->Subject = '"Гончаренко Анастасия"';
+    $mail->setFrom('nastyagoncharenko5@gmail.com', 'Гончаренко Анастасия');
+    $mail->addAddress('nastyagoncharenko5@gmail.com');
+    $mail->Subject = '"Гончаренко Анастасия"';
 
-$body = '<h1>Встречайте супер письмо!</h1>';
+    $body = '<h1>Встречайте супер письмо!</h1>';
 
-if(trim(!empty($_POST['name']))){
-    $body.='<p><strong>Name:</strong> '.$_POST['name'].'</p>';
-}
-if(trim(!empty($_POST['phone']))){
-    $body.='<p><strong>Phone:</strong> '.$_POST['phone'].'</p>';
-}
-if(trim(!empty($_POST['email']))){
-    $body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
-}        
-if(trim(!empty($_POST['message']))){
-    $body.='<p><strong>Comment:</strong> '.$_POST['message'].'</p>';
-}
+    if(trim(!empty($_POST['name']))){
+        $body.='<p><strong>Name:</strong> '.$_POST['name'].'</p>';
+    }
+    if(trim(!empty($_POST['phone']))){
+        $body.='<p><strong>Phone:</strong> '.$_POST['phone'].'</p>';
+    }
+    if(trim(!empty($_POST['email']))){
+        $body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+    }
+    if(trim(!empty($_POST['message']))){
+        $body.='<p><strong>Comment:</strong> '.$_POST['message'].'</p>';
+    }
 
-$mail->Body = $body;
+    $mail->Body = $body;
 
-if (!$mail->send()) {
-    $message = 'Error';
-} else {
-    $message = 'Succes!'
-}
+    if (!$mail->send()) {
+        $message = 'Error';
+    } else {
+        $message = 'Succes!'
+    }
 
-$response = ['message' => $message];
+    $response = ['message' => $message];
 
-header('Content-type: application/json')
-echo json_encode($response);
+    header('Content-type: application/json');
+    echo json_encode($response);
 ?>
